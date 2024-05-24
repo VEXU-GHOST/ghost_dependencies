f4b08d17949b0d86d275d9395afbdbc91d4e5338
xwilson03 <xanderwilson2014@gmail.com>
2024-05-24 05:12:31 -0500

    replaces copy_src script with new prepare_pkg script

---
+++ scripts/prepare_pkg.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+print_help() {
+    echo "Usage: $0 [pkg_name]"
+    echo
+    echo "Prepares the given package for building."
+    echo
+    echo
+    echo "--- OPTIONS ---"
+    echo 
+    echo "PKG_NAME (string):"
+    echo "  Determines the package to build. Must have a matching filelist!"
+    echo "  Output package name will be wrapped by 'ghost-' and the invoking machine architecture."
+    echo "  Example: '$0 casadi' on an x86_64 machine will prepare 'ghost-casadi-amd64'."
+}
+
+# Check if help option set
+for arg in $@; do
+    if [[ $arg =~ ^--help$ ]]; then
+        print_help
+        exit 0
+    fi
+done
+
+# Require repo base as current dir
+if [[ ! $(pwd) == *"ghost_dependencies" ]]; then
+    echo "Failure: script must be ran in ghost_dependencies base directory."
+    exit -1
+fi
+
+# Check argument count
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 [pkg_name]"
+    exit -1
+fi
+
+pkg=$1
+arch=$(dpkg --print-architecture)
+
+# Check if package list file exists
+if [[ ! -f ./lists/$pkg ]]; then
+    echo "Failure: missing list file for package $pkg."
+    exit -1
+fi
+
+# Remove old files
+find ./src/ghost-$pkg-$arch -mindepth 1 -maxdepth 1 | grep -v DEBIAN | xargs rm -rf
+
+# Copy package files
+mode=d
+while read line; do
+
+    # Switch from mkdir to cp upon reaching a "---"
+    if [[ $line =~ --- ]]; then
+        mode='f'
+        continue
+    fi
+
+    case $mode in
+        'd')
+            mkdir -p src/ghost-$pkg-$arch$line
+            ;;
+        'f')
+            cp -r $line src/ghost-$pkg-$arch$line
+            ;;
+    esac
+
+done < "./lists/$pkg"
